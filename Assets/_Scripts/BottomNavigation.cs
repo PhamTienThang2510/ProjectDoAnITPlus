38606e3cb7e73f44834b1fc180bea32250c70621
Phạm Tiến Thắng <132671977+PhamTienThang2510@users.noreply.github.com>
2026-04-21 19:03:57 +0700

    Initial commit

---
+++ Assets/_Scripts/BottomNavigation.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomNavigation : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> GroupPanels;
+    [SerializeField] private List<Button> GroupButtons;
+    [SerializeField] private GameObject Indicator;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Initialize the bottom navigation
+        InitializeBottomNavigation();
+    }
+
+    private void InitializeBottomNavigation()
+    {
+        // Set up button listeners
+        for (int i = 0; i < GroupButtons.Count; i++)
+        {
+            int index = i; // Capture the current index
+            GroupButtons[i].onClick.AddListener(() => OnGroupButtonClicked(index));
+        }
+
+        // Show the first panel by default
+        ShowPanel(2);
+    }
+
+    private void OnGroupButtonClicked(int index)
+    {
+        ShowPanel(index);
+    }
+
+    private void ShowPanel(int index)
+    {
+        // Hide all panels
+        foreach (var panel in GroupPanels)
+        {
+            panel.SetActive(false);
+        }
+
+        // Show the selected panel
+        if (index >= 0 && index < GroupPanels.Count)
+        {
+            GroupPanels[index].SetActive(true);
+            // Move the indicator to the selected button
+            Indicator.transform.localPosition = new Vector3(GroupButtons[index].transform.localPosition.x, Indicator.transform.localPosition.y, 0);
+        }
+    }
+}
