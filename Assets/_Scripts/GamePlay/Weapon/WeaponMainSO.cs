32c32258edc5df243d02f99e0aafc60acf28e7d9
Phạm Tiến Thắng <132671977+PhamTienThang2510@users.noreply.github.com>
2026-04-23 00:46:23 +0700

    basic weaponSO
    basic weaponSO

---
+++ Assets/_Scripts/GamePlay/Weapon/WeaponMainSO.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponMainSO", menuName = "Scriptable Objects/WeaponMainSO")]
+public class WeaponMainSO : ScriptableObject
+{
+    public List<WeaponLevel> weaponLevels;
+    public WeaponSub weaponSubs;
+}
+
+[CreateAssetMenu(fileName = "WeaponLevel", menuName = "Scriptable Objects/WeaponLevel")]
+public class WeaponLevel : ScriptableObject
+{
+    public int level;
+    public float damage;
+    public float cooldown;
+    public float range;
+}
+
+[CreateAssetMenu(fileName = "WeaponSub", menuName = "Scriptable Objects/WeaponSub")]
+public class WeaponSub : ScriptableObject
+{
+    public int level;
+    public TypeWeaponSub typeWeaponSub;
+    public float bonus;
+}
+[CreateAssetMenu(fileName = "WeaponSubSO", menuName = "Scriptable Objects/WeaponSubSO")]
+public class WeaponSubSO : ScriptableObject
+{
+    public List<WeaponSub> weaponSubLevels;
+}
+public enum TypeWeaponSub
+{
+    speed,
+    damage,
+    cooldown,
+    criticalChance,
+    hp
+}
---
+++ Assets/_Scripts/GamePlay/Weapon/Weapon_Base.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Weapon_Base : MonoBehaviour
+{
+    public WeaponType weaponType;
+
+}
+
+public enum WeaponType
+{
+    main,
+    sub
+}
